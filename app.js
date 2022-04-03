53b9eac2b02adb5348a9d0cccdd636fdaf21ef61
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-04-03 22:59:19 +0800

    update

---
+++ app.js
@@ -0,0 +1,36 @@
+// Variables and Values
+
+let js = 'amazing'
+console.log(js)
+console.log(48 + 8 + 30 - 10)
+
+console.log('Jie')
+console.log(25)
+
+// string
+let firstName = 'jie'
+firstName = 'Benjie'
+// number
+var age = 24
+age = 25
+//boolean
+const graduate = 'true'
+// undefined
+let job
+// null
+let address = null
+
+console.log(`My name is ${firstName} i'm ${age} graduate ${graduate}`)
+
+// let = can be re-assign *Block
+// var = can be re-assign *Function
+// const = can't be re-assign
+
+// Identify Data Types
+
+console.log(typeof firstName) // string
+console.log(typeof age) // number
+console.log(typeof graduate) // boolean
+console.log(typeof job) // undefined
+console.log(typeof address) // null
+
