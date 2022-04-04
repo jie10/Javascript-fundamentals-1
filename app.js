f42a3d04bb54fea1237f5b0a74c1cc06f6683698
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-04-04 21:47:40 +0800

    code challenge

--- app.js
+++ app.js
@@ -60,3 +60,24 @@ console.log(ageJie > 25) // false
 console.log(ageJie >= 25) // true
 console.log(ageAngel === 26) // true
 console.log(dateNow - 1997 > dateNow - 1996) // false
+
+/*
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+    BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+    1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+    Test data:
+    § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+    GOOD LUCK 😀
+    */
+
